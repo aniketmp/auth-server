a6d10208d1f4ced709d9a9696d811e3337f4744f
Unknown <aniket.paranjpe87@gmail.com>
2019-04-20 16:21:41 +0530

    authorization code working all all other previous

--- src/main/java/no/tusla/Oauth2Config.java
+++ src/main/java/no/tusla/Oauth2Config.java
@@ -2,46 +2,28 @@ package no.tusla;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 @Configuration
-@EnableAuthorizationServer
 public class Oauth2Config  extends AuthorizationServerConfigurerAdapter
-{
+{	
+	
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	@Autowired
-	private UserDetailsService userDetailsService;
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
+	}
 	
 	 @Override
      public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory().withClient("springsecurity")
         		.secret("Teno2019")
                 .authorizedGrantTypes("client_credentials","password","authorization_code")	    //add refresh_token grant if you need refresh token              
-                .scopes("webclient", "mobilecclient");
+                .scopes("webclient", "mobileclient");
     }
-	 
-	  /*@Override
-	    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-	        clients.inMemory()
-	               .withClient("fooClientId")
-	               .secret("secret")
-	               .authorizedGrantTypes("authorization_code")
-	               .scopes("foo", "read", "write")
-	               .redirectUris("http://localhost:8080/redirect");
-	  }*/
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-    	endpoints.authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService);        
-    }
-    
-      
+	 	   
 }
--- src/main/java/no/tusla/WebSecurityConfigurer.java
+++ src/main/java/no/tusla/WebSecurityConfigurer.java
@@ -1,45 +1,14 @@
 package no.tusla;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
-import org.springframework.boot.autoconfigure.security.SecurityProperties; 
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter; 
 @Configuration
-@EnableWebSecurity
-//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER) //Only in case of authorization_code grant type, this needs to be commented.
-public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter
+public class WebSecurityConfigurer extends GlobalAuthenticationConfigurerAdapter
 {
+	
 	@Override
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception
-	{
-		return super.authenticationManagerBean();
-	}
-	@Override
-	@Bean
-	public UserDetailsService userDetailsServiceBean() throws Exception {
-		return super.userDetailsServiceBean();
-	}
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-        .csrf().disable()        
-        .authorizeRequests()
-        .and()
-        .formLogin()
-        .permitAll().and().logout().permitAll();
-    }
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception 
-	{
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 		.withUser("aniket")
 		.password("aniket")
@@ -60,7 +29,6 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter
 		.withUser("henrik")
 		.password("henrik")
 		.roles("USER", "ADMIN");
-	}
-	
+	}  
 	 
 }
